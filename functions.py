251fe6045a60bfb53ae2475d4010e514c438261c
Uram <urmnisi@gmail.com>
2021-03-08 20:31:07 +0200

    Add files via upload
    Draw using a function

---
+++ functions.py
@@ -0,0 +1,12 @@
+
+def draw_grid():
+    str = "-" * 4
+    print("+" + str + "+" + str + "+")
+
+    str2 = "|" + "  " * 2
+    print(str2 * 3)
+    print(str2 * 3)
+    print(str2 * 3)
+    print(str2 * 3)
+
+draw_grid()
